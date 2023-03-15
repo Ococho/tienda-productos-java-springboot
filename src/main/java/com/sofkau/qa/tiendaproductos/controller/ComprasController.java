eb8e80d97c3b9ae21982ff8e90353c6a6fdb4b7b
Ococho <jonathan.vargas85@gmail.com>
2023-03-15 09:25:48 -0500

    ✅ 🚨  2

--- src/main/java/com/sofkau/qa/tiendaproductos/controller/ComprasController.java
+++ src/main/java/com/sofkau/qa/tiendaproductos/controller/ComprasController.java
@@ -8,18 +8,23 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/compras")
+@RequestMapping("api/v1/compras/")
 public class ComprasController {
 
     @Autowired
     private IComprasService service;
 
 
-    @GetMapping()
-    public ResponseEntity obtenerFacturas(){
+    @GetMapping("{variable}")
+    public ResponseEntity obtenerFacturas(@PathVariable("variable") String variable){
+        // viernes, 3 de febrero 12:53 a. m.
+
+        System.out.println(new SimpleDateFormat("EEEE, d 'de' MMMM hh:mm a").format(new Date()));
         return new ResponseEntity(service.obtenerFacturas(), HttpStatus.FOUND);
     }
 
